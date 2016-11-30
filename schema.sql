9bc65c3f8f02c6166e1390d29f4d4ed608c9c843
Hannah <hannahjmccain@gmail.com>
2016-11-30 15:00:29 -0500

    Updated schema.sql

--- schema.sql
+++ schema.sql
@@ -1,16 +1,36 @@
-CREATE TABLE IF NOT EXISTS recipes (
-	id SERIAL PRIMARY KEY,
-	name TEXT NOT NULL,
-	description TEXT NOT NULL
-);
-
-INSERT INTO recipes (name, description)
-VALUES ('Khichidi Kadhi', 'Rice and lentils with a yoghurt gravy');
-
-INSERT INTO recipes (name, description)
-VALUES ('Pizza', 'Cheese and marinara on dough');
-
-INSERT INTO recipes (name, description)
-VALUES ('Macaroni and Cheese', 'Pasta, cheddar cheese and milk');
-
+-- CREATE TABLE IF NOT EXISTS recipes (
+-- 	id SERIAL PRIMARY KEY,
+-- 	name TEXT NOT NULL,
+-- 	description TEXT NOT NULL
+-- );
+--
 -- INSERT INTO recipes (name, description)
+-- VALUES ('Khichidi Kadhi', 'Rice and lentils with a yoghurt gravy');
+--
+-- INSERT INTO recipes (name, description)
+-- VALUES ('Pizza', 'Cheese and marinara on dough');
+--
+-- INSERT INTO recipes (name, description)
+-- VALUES ('Macaroni and Cheese', 'Pasta, cheddar cheese and milk');
+
+-- UPDATE recipes
+-- SET description='Delicious rice and lentils with a yogurt gravy'
+-- WHERE name='Khichidi Kadhi';
+--
+-- UPDATE recipes
+-- SET description='Cheese, marinara, and other ingredients of choice on baked dough'
+-- WHERE name='Pizza';
+--
+-- UPDATE recipes
+-- SET name='Mac & Cheese'
+-- WHERE name='Macaroni and Cheese';
+
+-- DELETE FROM recipes
+-- WHERE name='Pizza';
+
+-- CREATE TABLE IF NOT EXISTS tags (
+-- 	id SERIAL PRIMARY KEY,
+-- 	tag TEXT NOT NULL
+-- );
+
+-- DROP TABLE tags;
