cdecf8661aba4ff4b17147e32884d56539f9151c
Hannah <hannahjmccain@gmail.com>
2016-11-30 17:31:16 -0500

    Added recipe tags.

--- schema.sql
+++ schema.sql
@@ -1,6 +1,3 @@
---ALLEN WAS HERE FIRST
---ALLEN WAS HERE AGAIN
-
 -- CREATE TABLE IF NOT EXISTS recipes (
 -- 	id SERIAL PRIMARY KEY,
 -- 	name TEXT NOT NULL,
@@ -29,7 +26,10 @@
 -- WHERE name='Macaroni and Cheese';
 
 -- DELETE FROM recipes
--- WHERE name='Pizza';
+-- WHERE id=4;
+--
+-- DELETE FROM recipes
+-- WHERE id=5;
 
 -- CREATE TABLE IF NOT EXISTS tags (
 -- 	id SERIAL PRIMARY KEY,
@@ -38,23 +38,76 @@
 
 -- DROP TABLE steps;
 
--- CREATE TABLE IF NOT EXISTS pizzaRecipeSteps (
+-- CREATE TABLE IF NOT EXISTS tags (
 -- 	id SERIAL PRIMARY KEY,
--- 	step TEXT NOT NULL,
--- 	recipes_id INTEGER REFERENCES recipes
+-- 	tag TEXT NOT NULL
 -- );
+--
+-- INSERT INTO tags (tag)
+-- VALUES ('Cheesy!');
+--
+-- INSERT INTO tags (tag)
+-- VALUES ('Comforting!');
+--
+-- INSERT INTO recipeSteps (step, recipes_id)
+-- VALUES ('Top with cheese', 6);
+--
+-- INSERT INTO recipeSteps (step, recipes_id)
+-- VALUES ('Add other toppings', 6);
+--
+-- INSERT INTO recipeSteps (step, recipes_id)
+-- VALUES ('Bake at 500 degrees F', 6);
+--
+-- INSERT INTO recipeSteps (step, recipes_id)
+-- VALUES ('Boil pasta', 3);
+--
+-- INSERT INTO recipeSteps (step, recipes_id)
+-- VALUES ('Make roux', 3);
+--
+-- INSERT INTO recipeSteps (step, recipes_id)
+-- VALUES ('Add shredded cheese to roux', 3);
+--
+-- INSERT INTO recipeSteps (step, recipes_id)
+-- VALUES ('Mix pasta and cheese sauce', 3);
+--
+-- INSERT INTO recipeSteps (step, recipes_id)
+-- VALUES ('Bake at 350 degrees F', 3);
 
-INSERT INTO pizzaRecipeSteps (step)
-VALUES ('Make pizza dough');
+-- DROP TABLE recipeSteps;
 
-INSERT INTO pizzaRecipeSteps (step)
-VALUES ('Add sauce');
+-- SELECT recipes.name, recipeSteps.step
+-- FROM recipes
+-- LEFT OUTER JOIN recipeSteps
+-- ON recipeSteps.recipes_id = recipes.id;
 
-INSERT INTO pizzaRecipeSteps (step)
-VALUES ('Add cheese');
+-- create table if not exists recipe_tags (
+--     recipe_id integer references recipes,
+--     tag_id integer references tags,
+--     primary key (recipe_id, tag_id)
+-- );
+--
+-- INSERT INTO recipe_tags (recipe_id, tag_id)
+-- VALUES (3, 1);
+--
+-- INSERT INTO recipe_tags (recipe_id, tag_id)
+-- VALUES (3, 2);
+--
+-- INSERT INTO recipe_tags (recipe_id, tag_id)
+-- VALUES (6, 1);
+--
+-- INSERT INTO recipe_tags (recipe_id, tag_id)
+-- VALUES (1, 2);
 
-INSERT INTO pizzaRecipeSteps (step)
-VALUES ('Add toppings');
 
-INSERT INTO pizzaRecipeSteps (step)
-VALUES ('Bake at 500 degrees F');
+
+
+
+
+
+
+
+
+
+
+
+--x
